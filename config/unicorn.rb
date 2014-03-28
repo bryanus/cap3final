6cb06d1ddc35fe9fe2f2697481ad8820380862ff
Bryan Heu <teamriab@gmail.com>
2014-03-28 11:05:41 -0700

    nginx  and unicorn config files

---
+++ config/unicorn.rb
@@ -0,0 +1,16 @@
+root = "/home/deployer/apps/cap3final/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.cap3final.sock"
+worker_processes 2
+timeout 30
+
+# Force unicorn to look at the Gemfile in the current_path
+# otherwise once we've first started a master process, it
+# will always point to the first one it started.
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "/home/deployer/apps/cap3final/current/Gemfile"
+end
